5c2809530b2ea0bc9fedb0aae9139d607bcd6e1d
luisrodge <luisrodge@outlook.com>
2016-10-23 18:41:27 -0600

    altercation db

--- db/migrate/20161018023959_create_balances.rb
+++ db/migrate/20161018023959_create_balances.rb
@@ -2,7 +2,6 @@ class CreateBalances < ActiveRecord::Migration
   def change
     create_table :balances do |t|
       t.decimal :amount
-      t.references :shop, index: true, foreign_key: true
 
       t.timestamps null: false
     end
