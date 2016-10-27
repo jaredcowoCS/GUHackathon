06fa473264e45cf2cd74ca3620cb3d5a912e7e25
luisrodge <luisrodge@outlook.com>
2016-10-26 23:51:34 -0600

    uploader edit

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -6,6 +6,7 @@ class ProductImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   process resize_to_fit: [250, 250]
+  process :convert => 'png'
 
   version :normal do
     process :resize_to_fill => [240, 240]
